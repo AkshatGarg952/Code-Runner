01094ec7743ec669224a1f56d19f6564ffe879d7
akshat45 <gargakshat952@gmail.com>
2026-01-05 16:29:19 +0530

    Updated env file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { validateRunRequest } from './middleware/validator.js';
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || '*',
+  origin: true,
   credentials: true
 }));
 
